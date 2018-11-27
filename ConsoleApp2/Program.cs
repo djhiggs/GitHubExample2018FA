60b67c56ad62bc773d76d64adc77c82fb01ff5ab
Nothinggood87 <andrewrhands@gmail.com>
2018-11-27 14:00:20 -0600

    added another important calculator

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,14 +12,26 @@ namespace ConsoleApp2
             // This is my amazing codebase.
             // How amazing can it be tho?
             double pi = 0;
+            double e = 0;
             for (int i = 0; i < 1000*1000; i++)
             {
                 pi += (i % 2 == 0 ? 4d : -4d) / (2 * i + 1);
-                Console.WriteLine("PI Approx = " + pi);
+                var add = 1 / Factorial(i);
+                if(add > 0 && !double.IsNaN(add) && !double.IsInfinity(add))
+                    e += add;
+                Console.WriteLine("PI Approx = " + pi + ", e Approx = " + e);
                 // I like pie...
                 // Cake is better
             }
+
             Console.ReadKey();
+        }
+        static double Factorial(int n)
+        {
+            double factorial = 1;
+            for (int i = 1; i <= n; i++)
+                factorial *= i;
+            return factorial;
         }
     }
 }
